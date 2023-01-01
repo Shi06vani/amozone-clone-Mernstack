a76b562e62cfd49e9b56c509b942b2c94647b929
Shi06vani <103645374+Shi06vani@users.noreply.github.com>
2023-01-01 15:57:45 +0700

    Add files via upload

---
+++ src/component/Mainsection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import Carousel from "react-multi-carousel";
+import Slide from "./Slide";
+import Homepage from "./Homepage";
+import Cartsection from "./Cartsection";
+
+export default function Mainsection(){
+
+    return(
+
+        <>
+           <Homepage/>
+           <Slide/>
+          <Cartsection/>
+          
+        </>
+    );
+}
