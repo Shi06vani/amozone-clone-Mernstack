3954194d64343878a24bc097d76c732e800e1da1
Shi06vani <103645374+Shi06vani@users.noreply.github.com>
2023-01-02 14:36:05 +0700

    Add files via upload

---
+++ src/component/buynow/Subtotal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Subtotal() {
+  return (
+    <div className='sub_iteam'>
+      <h4 style={{borderTop:"1px solid gray", display:"flex" ,justifyContent:"flex-end",alignItems:"center", padding:"10px"}}> 
+        subtotal( 1 iteam: ) <span style={{  fontWeight:700,color:"#111" ,fontSize:"20px" }}>4049.00</span>
+      </h4>
+    </div>
+  )
+}
